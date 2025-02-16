e1a98db42b16f0306f7996273b3601d0fa9bddb1
bkuker <bkuker@billkuker.com>
2025-02-16 13:49:49 -0500

    Layout

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,25 @@ import Worksheet from './components/Worksheet.vue'
 </script>
 
 <template>
-  <Worksheet id="worksheet"/>
-  <Tape id="tape"></Tape>
+  <div id="app">
+    <Worksheet id="worksheet"/>
+    <Tape id="tape"></Tape>
+  </div>
 </template>
 
 <style scoped>
+#app {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+
 #tape {
   position: absolute;
   left: 10in;
   top: 1in;
 }
+
 div {
   filter: drop-shadow(5px 5px 5px #666);
 }
