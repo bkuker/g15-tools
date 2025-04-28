40d9ab4fb6e33ba85fbb499534a3a388341572c2
bkuker <bkuker@billkuker.com>
2025-04-28 17:06:39 -0400

    use ptr for mandelbrot

--- programs/mandlebrot/mandelbrot.bat
+++ programs/mandlebrot/mandelbrot.bat
@@ -1,2 +1,3 @@
+del mandelbrot.ptr
 del mandelbrot.pti
-npm run asm -- --bootable mandelbrot.asm > mandelbrot.pti && python C:\Users\bkuker\Desktop\BendixG15-lah_python\emulators\python\g15d\g15d.py mandelbrot.txt
+npm run asm -- --bootable --ptr mandelbrot.asm > mandelbrot.ptr && python C:\Users\bkuker\Desktop\BendixG15-lah_python\emulators\python\g15d\g15d.py mandelbrot.txt
