0e35f4f30731fb08a7c739e3f34db9bdeecd2c71
bkuker <bkuker@billkuker.com>
2025-10-10 11:21:36 -0400

    julia file name updates

--- programs/julia/julia.asm
+++ programs/julia/julia.asm
@@ -3,10 +3,10 @@ ct:                     Count
 .   d3
 
 <BLOCK>                 Block 2 Fractal Code
-#include "fractal.asm"
+#include "julia-fractal.asm"
 
 <BLOCK>                 Block 1
 #include "../lib/complex.mult.div.asm"
 
 <BLOCK>                 Block Raster Code
-#include "rastr.asm"
+#include "julia-rastr.asm"
